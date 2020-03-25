2acd84748855185b4d095d37c57e1f62d9696de2
kimia <mk_ghassemi76@yahoo.com>
2020-03-25 11:02:56 +0430

    section 2 p 11

--- App.js
+++ App.js
@@ -20,14 +20,28 @@ import {
 
 
 export default function App() {
- 
+ const [enteredGoal , setEnteredGoal] = useState('')
+ const [courseGoals , setCourseGoals] = useState('')
+ const gaoalInputHandler = (enteredText) =>{
+  setEnteredGoal(enteredText)
+ };
+
+ const addGoalHandler = () =>{
+  //  setCourseGoals({...courseGoals , enteredGoal})
+  setCourseGoals(currentGoals => [...currentGoals , enteredGoal])
+
+ }
+
   return (
-  <View style={{padding : 30}}>
-    <View style={{flexDirection: 'row' , justifyContent: 'space-between' , alignItems:'center'}}>
+  <View style={styles.screen}>
+    <View style={styles.inutContainer}>
       <TextInput
        placeholder="CourseGoal"
-       style={{ width:'80%' , borderColor:'black' , borderWidth:1}}/>
-      <Button title="Add"/>
+       style={styles.input}
+       onChangeText={gaoalInputHandler}
+       value = {enteredGoal}/>
+      <Button title="Add"
+      onPress={addGoalHandler}/>
     </View>
     <View>
 
@@ -38,6 +52,20 @@ export default function App() {
 };
 
 const styles = StyleSheet.create({
+  screen :{
+    padding :50
+  },
+  input:{
+    width:'80%' , 
+    borderColor:'black' ,
+    borderWidth:1
+  },
+  inutContainer:{
+    flexDirection: 'row' , 
+    justifyContent: 'space-between' , 
+    alignItems:'center'
 
+  }
+  
 });
 
